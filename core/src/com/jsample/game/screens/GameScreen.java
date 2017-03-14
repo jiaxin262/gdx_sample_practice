88cd25d02438ecfa8659e2e7745bfed258dd1f5c
jiaxin <jiaxin@jiaxindeMacBook-Pro.local>
2017-03-14 17:40:19 +0800

    add

--- core/src/com/jsample/game/screens/GameScreen.java
+++ core/src/com/jsample/game/screens/GameScreen.java
@@ -10,22 +10,32 @@ package com.jsample.game.screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.jsample.game.MyGdxGame;
 
 public class GameScreen implements Screen {
     public static final String TAG = "GameScreen";
 
     private Game game;
     private Stage stage;
+    private Stage uiStage;
     private OrthographicCamera camera;
+    private Slider slider;
+    private InputMultiplexer multiplexer;
 
     private float currentX, currentY, currentZ;
     private float imgWidth, imgHeight;
@@ -36,24 +46,66 @@ public class GameScreen implements Screen {
     public GameScreen(Game game) {
         this.game = game;
         stage = new Stage(new ScreenViewport());
+        uiStage = new Stage(new ScreenViewport());
         Gdx.input.setCatchBackKey(true);
         Gdx.input.setCatchMenuKey(true);
+
+        multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(uiStage);
+        multiplexer.addProcessor(stage);
 
         camera = (OrthographicCamera) stage.getCamera();
         currentX = camera.position.x;
         currentY = camera.position.y;
         currentZ = camera.zoom;
+
         Image image = new Image(new Texture(Gdx.files.internal("map.jpg")));
         imgWidth = image.getWidth();
         imgHeight = image.getHeight();
-        stage.addActor(image);
         minZoom = imgWidth / Gdx.graphics.getWidth();
+        image.addListener(new ActorGestureListener() {
+            @Override
+            public void pan(InputEvent event, float x, float y, float deltaX, float deltaY) {
+                //Gdx.app.log(TAG, "deltaX:" + deltaX + ",deltaY:" + deltaY);
+            }
+        });
+        stage.addActor(image);
+
+        slider = new Slider(0.3f, minZoom, 0.01f, true, MyGdxGame.skin);
+        slider.setAnimateInterpolation(Interpolation.smooth);
+        slider.setHeight(Gdx.graphics.getHeight()*0.8f);
+        slider.setWidth(slider.getWidth() * 2);
+        slider.setPosition(20, Gdx.graphics.getHeight()/10);
+        slider.setValue((0.3f + minZoom) / 2);
+        currentZ = slider.getValue();
+        slider.addListener(new InputListener(){
+            @Override
+            public void touchDragged(InputEvent event, float x, float y, int pointer) {
+                camera.zoom = 0.3f + minZoom - slider.getValue();
+                currentZ = camera.zoom;
+                camera.update();
+                Gdx.app.log(TAG,"dragged-slider Value:"+slider.getValue());
+                super.touchDragged(event, x, y, pointer);
+            }
+
+            @Override
+            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.log(TAG, "up-slider Value:"+slider.getValue());
+            }
+            @Override
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.log(TAG, "down-slider Value:"+slider.getValue());
+                return true;
+            }
+        });
+        uiStage.addActor(slider);
 
     }
 
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(new GestureDetector(new MapInputListener()));
+        //Gdx.input.setInputProcessor(new GestureDetector(new MapInputListener()));
+        Gdx.input.setInputProcessor(multiplexer);
     }
 
     @Override
@@ -65,14 +117,16 @@ public class GameScreen implements Screen {
             Gdx.app.log(TAG, "back-pressed");
             game.setScreen(new TitleScreen(game));
         }
+        uiStage.act();
         stage.act();
         stage.draw();
+        uiStage.draw();
     }
 
     private void moveCamera() {
         camera.position.x = currentX;
         camera.position.y = currentY;
-        camera.zoom = currentZ;
+        //camera.zoom = currentZ;
         camera.update();
     }
 
@@ -99,6 +153,7 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         stage.dispose();
+        uiStage.dispose();
     }
 
     class MapInputListener implements GestureDetector.GestureListener {
@@ -106,7 +161,7 @@ public class GameScreen implements Screen {
         @Override
         public boolean touchDown(float x, float y, int pointer, int button) {
             Gdx.app.log(TAG, "touchDown");
-            return true;
+            return false;
         }
 
         @Override
